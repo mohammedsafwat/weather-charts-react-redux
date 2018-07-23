6443246e2cb310cf79cd1d1ef9946e3a267aa216
Mohammed Safwat <mail.msafwat@gmail.com>
2018-07-24 01:50:46 +0200

    Create a new container 'SearchBar'.

---
+++ src/containers/search_bar.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
+
+class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {term: ''};
+
+        this.onInputChange = this.onInputChange.bind(this);
+        this.onFormSubmit = this.onFormSubmit.bind(this);
+    }
+
+    onInputChange(event) {
+        this.setState({ term: event.target.value });
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+
+        // We need to go and fetch weather data
+        this.props.fetchWeather(this.state.term);
+        this.setState({term: ''});
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onFormSubmit} className="input-group">
+                <input 
+                    placeholder="Get a five-day forecast for your favorite city." 
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange} />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
+            </form>
+        );
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators({fetchWeather}, dispatch);
+}
+
+// The 'null' as the first argument, it means that
+// we don't need any state here.
+export default connect(null, mapDispatchToProps)(SearchBar);
